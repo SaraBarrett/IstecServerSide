c44894c6ae10b66bbc4cbe70e044c932ea1c7e94
SaraBarrett <sara.c.mon@gmail.com>
2025-04-08 15:15:08 +0100

    form

--- resources/views/users/add_users.blade.php
+++ resources/views/users/add_users.blade.php
@@ -1,4 +1,26 @@
 @extends('layouts.fe_master')
 @section('content')
     <h5>Aqui, no futuro vais ter um formulário para adicionar utilizadores</h5>
+
+    <form>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Nome</label>
+            <input required name="name" type="text" class="form-control" id="exampleInputEmail1"
+                aria-describedby="emailHelp">
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email</label>
+                <input required name="email" type="email" class="form-control" id="exampleInputEmail1"
+                    aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input required name="password" type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 @endsection
