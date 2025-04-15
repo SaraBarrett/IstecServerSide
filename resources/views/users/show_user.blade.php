79b9bae5a532306cdb53dad60ece916f319dfb19
SaraBarrett <sara.c.mon@gmail.com>
2025-04-15 09:58:14 +0100

    update

--- resources/views/users/show_user.blade.php
+++ resources/views/users/show_user.blade.php
@@ -1,9 +1,29 @@
 @extends('layouts.fe_master')
 @section('content')
     <h5>Dados do User: {{ $ourUser->name }}</h5>
-    <p>Nome: {{ $ourUser->name }}</p>
-    <p>Morada:{{ $ourUser->address }}</p>
-    <p>Nif:{{ $ourUser->nif }}</p>
-    <p>Email:{{ $ourUser->email }}</p>
-    <p>Password:{{ $ourUser->password }}</p>
+    <form method="POST" action="">
+        @csrf
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Nome</label>
+            <input required name="name" value="{{ $ourUser->name }}" type="text" class="form-control"
+                id="exampleInputEmail1" aria-describedby="emailHelp">
+            @error('name')
+                Nome Inválido
+            @enderror
+
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email</label>
+                <input disabled name="email" value="{{ $ourUser->email }}" type="email" class="form-control"
+                    id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+
+            <label for="exampleInputEmail1" class="form-label">Morada</label>
+            <input  name="address" value="{{ $ourUser->address }}" type="text" class="form-control"
+                id="exampleInputEmail1" aria-describedby="emailHelp">
+
+            <label for="exampleInputEmail1" class="form-label">Nif</label>
+            <input  name="nif" value="{{ $ourUser->nif }}" type="text" class="form-control"
+                id="exampleInputEmail1" aria-describedby="emailHelp">
+            <button type="submit" class="btn btn-primary">Actualizar</button>
+    </form>
 @endsection
